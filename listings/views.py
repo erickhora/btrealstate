4e4d6c2159484114af7ece718bb240406450251a
Erick Hora <ennh@cin.ufpe.br>
2019-06-22 14:39:21 -0300

    - Adicionado funcionalidades de pesquisa no formulário de pesquisa; - Também adicionada algumas features de UX e filtros na pesquisa.

--- listings/views.py
+++ listings/views.py
@@ -27,11 +27,44 @@ def listing(request, listing_id):
   return render(request, 'listings/listing.html', context)
 
 def search(request):
+  queryset_list = Listing.objects.order_by('-list_date')
+
+  #keywords
+  if 'keywords' in request.GET:
+    keywords = request.GET['keywords']
+    if keywords:
+      queryset_list = queryset_list.filter(description__icontains=keywords)
+
+  #city
+  if 'city' in request.GET:
+    city = request.GET['city']
+    if city:
+      queryset_list = queryset_list.filter(city__iexact=city)
+  
+  #state
+  if 'state' in request.GET:
+    state = request.GET['state']
+    if state:
+      queryset_list = queryset_list.filter(state__iexact=state)
+
+  #bedromms
+  if 'bedromms' in request.GET:
+    bedromms = request.GET['bedromms']
+    if bedromms:
+      queryset_list = queryset_list.filter(bedromms__lte=bedromms)
+
+  #price
+  if 'price' in request.GET:
+    price = request.GET['price']
+    if price:
+      queryset_list = queryset_list.filter(price__lte=price)
 
   context = {
     'state_choices': state_choices,
     'bedroom_choices': bedroom_choices,
-    'price_choices': price_choices
+    'price_choices': price_choices,
+    'listings': queryset_list,
+    'values': request.GET
   }
 
   return render(request, 'listings/search.html', context)
